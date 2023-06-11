78bdbba7ef855281bc1426babfbbd2ddc64859f7
Aarow Epstein <aarowepstein1@gmail.com>
2023-06-10 17:24:39 -0700

    commit tree e78ca166e19fc434118705a7cd35e10c7cf6a142

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
+	
+    # setter method
+    def name=(name)
+      @name = name
+    end
+  
+    # getter method
+    def name
+      @name
+    end
 
-end
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  end
+  
+
